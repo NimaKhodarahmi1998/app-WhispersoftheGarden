9423e96b77aded559c96140895cf19a1c4cdb51c
Nima Khodarahmi <7798.nima@gmail.com>
2026-02-15 12:57:12 +0100

    Icon Added

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
             teamIdentifier: "HT99YC33BT",
             displayVersion: "1.0",
             bundleVersion: "1",
-            appIcon: .placeholder(icon: .sun),
+            appIcon: .asset("AppIcon"),
             accentColor: .presetColor(.teal),
             supportedDeviceFamilies: [
                 .pad,
